4bd37162936913e3825fac8cb87355f329564ee6
amackzie@gmail.com <amackzie@gmail.com>
2025-03-15 15:37:55 +0300

    navbar styling

--- packages/frontend/src/routes/__root.tsx
+++ packages/frontend/src/routes/__root.tsx
@@ -8,16 +8,20 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <>
-      <div className="p-2 flex gap-2 py-5">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{" "}
-        <Link to="/login" className="[&.active]:font-bold">
-          Login
-        </Link>
-        <Link to="/signup" className="[&.active]:font-bold">
-          Sign up
-        </Link>
+      <div className="p-2 flex gap-2 py-5 justify-between">
+        <div className="ml-5">
+          <Link to="/" className="[&.active]:font-bold">
+            Home
+          </Link>{" "}
+        </div>
+        <div className="mr-5 flex gap-4">
+          <Link to="/login" className="[&.active]:font-bold">
+            Login
+          </Link>
+          <Link to="/signup" className="[&.active]:font-bold">
+            Sign up
+          </Link>
+        </div>
       </div>
       <hr />
       <div className="flex flex-col items-center h-screen">
