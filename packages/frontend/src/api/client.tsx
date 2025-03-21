39bcbec45634463d4153b79001ff003b57a462a3
amackzie@gmail.com <amackzie@gmail.com>
2025-03-21 19:35:21 +0300

    make client functions reusable

--- packages/frontend/src/api/client.tsx
+++ packages/frontend/src/api/client.tsx
@@ -1,15 +1,26 @@
 import { newExpense } from "@/pages/newExpense";
 
-export async function addExpense(options: newExpense) {
-  const response = await fetch(
-    `${import.meta.env.VITE_API_BASE_URL}/api/expenses/newExpense`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(options),
-    }
-  );
+type sendRequestOptions<T> = {
+  url: string;
+  data: T;
+};
+
+const sendRequest = async <T,>(options: sendRequestOptions<T>) => {
+  const { url, data } = options;
+
+  const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/${url}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
   return response;
+};
+
+export async function addExpense(options: newExpense) {
+  return await sendRequest<newExpense>({
+    url: "api/expenses/newExpense",
+    data: options,
+  });
 }
