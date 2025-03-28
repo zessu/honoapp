14ee7ca2026f9d8c485995ad7d5e0ec2255d558c
amackzie@gmail.com <amackzie@gmail.com>
2025-03-28 15:21:50 +0300

    add loading skeletons during data fetching

--- packages/frontend/src/pages/dashboard.tsx
+++ packages/frontend/src/pages/dashboard.tsx
@@ -1,3 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import { getExpenses } from "@/api/client";
+
 export const Dashboard = () => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["expenses"],
+    queryFn: getExpenses,
+  });
+
+  if (isLoading)
+    return (
+      <div className="flex w-52 flex-col gap-4 mt-10">
+        <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton h-4 w-full"></div>
+      </div>
+    );
+  if (error)
+    return <div>Error loading expenses please try again later ...</div>;
+
   return <>Welcome to the dash</>;
 };
